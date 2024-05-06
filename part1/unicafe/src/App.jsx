7876ac43d4154d8fe0bdbcb48482f13beb4911db
Joseph BS <jjbs1805@gmail.com>
2024-05-05 19:25:21 -0500

    exersice 1.6: unicafe, step 1

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,58 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const handleClickGood = () => {
+    setGood(good + 1)
+    
+  }
+
+  const handleClickNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+
+  const handleClickBad = () => {
+    setBad(bad + 1)
+
+  }
+
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Give FeedBack</h1>
+
+      <Button handleClick = {handleClickGood} text = "good"></Button>
+      <Button handleClick = {handleClickNeutral} text = "neutral"></Button>
+      <Button handleClick = {handleClickBad} text = "bad"></Button>
+
+      <Statistics good = {good} neutral = {neutral} bad={bad}/>
+    </div>
   )
 }
 
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const Statistics = (props) =>(
+
+    <div>
+    <h1>Statistics</h1>
+    <p>good {props.good}</p>
+    <p>neutral {props.neutral}</p>
+    <p>bad {props.bad}</p>
+  </div>
+
+)
+
+
+
 export default App
+
