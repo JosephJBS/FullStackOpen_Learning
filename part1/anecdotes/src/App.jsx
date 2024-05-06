74f5cb80f29ba2ad4d9252fa024cb5d54b2b1eb9
Joseph BS <jjbs1805@gmail.com>
2024-05-05 21:17:28 -0500

    exersice 1.13: anecdotes, step 2

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,43 +1,46 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
-    'The only way to go fast, is to go well.'
-  ]
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
 
-  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
 
-  const clickNextAnecdote = () =>{
+  const [selected, setSelected] = useState(0);
+
+  const clickNextAnecdote = () => {
     const min = 0;
     const max = anecdotes.length;
     let randomInt = Math.floor(Math.random() * (max - min)) + min;
-    setSelected(randomInt)
-  }
+    setSelected(randomInt);
+  };
 
+  const clickVoteAnecdote = () => {
+    const newPoints = [...points];
+    newPoints[selected] += 1;
+    setPoints(newPoints);
+  };
 
   return (
     <div>
       {anecdotes[selected]}
-      <br />
-      <ButtonNextAnecdote text = "next anecdote" handleClick = {clickNextAnecdote} />
-
+      <p>has {points[selected]} votes</p>
+      <Button text="vote" handleClick={clickVoteAnecdote} />
+      <Button text="next anecdote" handleClick={clickNextAnecdote} />
     </div>
-    
+  );
+};
 
-  )
-}
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
-const ButtonNextAnecdote = (props) =>{
-  return(
-    <button onClick={props.handleClick}>{props.text}</button>
-  )
-}
-
-export default App
+export default App;
