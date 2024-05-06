4dea86461ab71856dad53a2d490107eb3d07bed4
Joseph BS <jjbs1805@gmail.com>
2024-05-05 19:41:25 -0500

    exersice 1.8 - 1.9 : unicafe, step 3-4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,68 +1,66 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
   // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleClickGood = () => {
-    setGood(good + 1)
-    
-  }
+    setGood(good + 1);
+  };
 
   const handleClickNeutral = () => {
-    setNeutral(neutral + 1)
-  }
+    setNeutral(neutral + 1);
+  };
 
   const handleClickBad = () => {
-    setBad(bad + 1)
-
-  }
-
-
+    setBad(bad + 1);
+  };
 
   return (
     <div>
       <h1>Give FeedBack</h1>
 
-      <Button handleClick = {handleClickGood} text = "good"></Button>
-      <Button handleClick = {handleClickNeutral} text = "neutral"></Button>
-      <Button handleClick = {handleClickBad} text = "bad"></Button>
+      <Button handleClick={handleClickGood} text="good"></Button>
+      <Button handleClick={handleClickNeutral} text="neutral"></Button>
+      <Button handleClick={handleClickBad} text="bad"></Button>
 
-      <Statistics good = {good} neutral = {neutral} bad={bad}/>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-  )
-}
+  );
+};
 
 const Button = (props) => (
-  <button onClick={props.handleClick}>
-    {props.text}
-  </button>
-)
+  <button onClick={props.handleClick}>{props.text}</button>
+);
 
-const Statistics = (props) =>{
+const Statistics = (props) => {
+  let all = props.good + props.neutral + props.bad;
+  let average = (props.good * 1 + props.neutral * 0 + props.bad * -1) / all;
+  let positive = (props.good / all) * 100;
 
-  let all = props.good + props.neutral + props.bad 
-  let average = (props.good*1 + props.neutral*0 + props.bad*-1)/all
-  let positive = props.good / all * 100
-  
+  if (all == 0) {
+    return (
+      <div>
+        <h1>Statistics</h1>
+        <p>No feedBack given</p>
+      </div>
+    );
+  }
+
   return (
     <div>
-    <h1>Statistics</h1>
-    <p>good {props.good}</p>
-    <p>neutral {props.neutral}</p>
-    <p>bad {props.bad}</p>
+      <h1>Statistics</h1>
+      <p>good {props.good}</p>
+      <p>neutral {props.neutral}</p>
+      <p>bad {props.bad}</p>
 
-    <p>all {all}</p>
-    <p>average {average} </p>
-    <p>positive {positive} % </p>
+      <p>all {all}</p>
+      <p>average {average} </p>
+      <p>positive {positive} % </p>
+    </div>
+  );
+};
 
-  </div>
-  )
-}
-
-
-
-export default App
-
+export default App;
