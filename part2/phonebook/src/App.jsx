1b64b8a715b1e01264582fd2d0315acdb549025f
Joseph BS <jjbs1805@gmail.com>
2024-05-14 14:45:10 -0500

    exercise 2.15 : phonebook step 10

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -19,8 +19,12 @@ const App = () => {
       number: newNumber,
     };
 
-    if (persons.find((person) => person.name === newName)) {
-      alert(newName + " is already added to phonebook");
+    if (personExistByName(newName)) {
+      console.log("si existe")
+      if(window.confirm(`${newName} is alredy added to phonebook, replace the old number with a new one ?`)){
+        const personFind = persons.find((person) => person.name === newName);
+        updateNumber(personFind.id);
+      }
     } else {
       personService.create(phoneObject).then((response) => {
         console.log(response);
@@ -68,6 +72,25 @@ const App = () => {
       }
     };
   };
+
+  const updateNumber = (id) => {
+    const person = persons.find( p => p.id === id )
+    const changePerson = {...person, number: newNumber}  
+
+    personService
+    .update(id,changePerson)
+    .then((returnedPerson)=>{
+      setPersons(persons.map(person => person.id != id ? person : returnedPerson))
+    })
+    .catch(error => {
+      alert(
+        `the note '${person.name}' was already deleted from server`
+      )
+      setPersons(person.filter(p => p.id !== id))
+    })
+  };
+
+  const personExistByName = (nameUwu) => persons.some((person) => person.name === nameUwu)
 
   return (
     <div>
